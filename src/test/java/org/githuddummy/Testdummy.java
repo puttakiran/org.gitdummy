24cf0fcfffdcd7718bec8771d0f2588d0427e8b0
HP <HP@DESKTOP-CSM5G2E>
2022-04-12 11:59:39 +0530

    program finish

---
+++ src/test/java/org/githuddummy/Testdummy.java
@@ -0,0 +1,16 @@
+package org.githuddummy;
+
+public class Testdummy 
+{
+
+	public static void main(String[] args)
+	{
+		int x=10;
+		int y=20;
+		int z=x+y;
+		System.out.println(z);
+		
+
+	}
+
+}
